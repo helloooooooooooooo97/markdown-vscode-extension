068ffff126569736c3256498c1ddfddac814eb08
steveacen <348876414@qq.com>
2025-08-18 23:29:44 +0800

    更新 InlineCode 组件，修改样式以使用 span 标签替代 code 标签，提升代码显示效果和用户体验。

--- webview/src/components/markdown/InlineCode.tsx
+++ webview/src/components/markdown/InlineCode.tsx
@@ -1,17 +1,10 @@
 // 行内代码组件
 export const InlineCode: React.FC<{ children: React.ReactNode }> = ({
-    children,
+  children,
 }) => (
-    <code
-        style={{
-            background: "#f6f8fa",
-            borderRadius: "4px",
-            padding: "2px 4px",
-            fontSize: "95%",
-        }}
-    >
-        {children}
-    </code>
+  <span className="bg-[#151111] rounded px-1 py-1 text-red-800">
+    {children}
+  </span>
 );
 
 export default InlineCode;
